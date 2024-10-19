73c7113aa8d40fa800e892d04cb74844f5a979b2
Anderson Souza <agsvip@yahoo.com.br>
2024-10-19 19:11:28 -0300

    commit 10

--- assets/js/scrits.js
+++ assets/js/scrits.js
@@ -53,3 +53,30 @@ window.onload = function() {
   slides.style.transform = `translateX(-${currentIndex * 100}%)`; // Define a posição inicial
   startAutoSlide();
 };
+
+
+// LGPD
+// Função para verificar se o usuário já aceitou ou recusou a política de privacidade
+document.addEventListener('DOMContentLoaded', function () {
+  const popup = document.getElementById('lgpd-popup');
+  const acceptBtn = document.getElementById('accept-btn');
+  const declineBtn = document.getElementById('decline-btn');
+
+  // Verifica se o usuário já aceitou ou recusou cookies
+  if (!localStorage.getItem('lgpdAccepted')) {
+      popup.style.display = 'block';
+  }
+
+  // Quando o usuário clica em "Aceitar"
+  acceptBtn.addEventListener('click', function () {
+      localStorage.setItem('lgpdAccepted', 'true');
+      popup.style.display = 'none';
+  });
+
+  // Quando o usuário clica em "Recusar"
+  declineBtn.addEventListener('click', function () {
+      localStorage.setItem('lgpdAccepted', 'false');
+      popup.style.display = 'none';
+  });
+});
+
